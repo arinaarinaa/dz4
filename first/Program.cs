24fd8e7771f320124469c8055b72e4e92c1525df
arinach <arina.tchukurova@yandex.ru>
2022-09-01 09:28:49 +0300

    add first

---
+++ first/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("2 цифры  - ");
+double n = Convert.ToDouble(Console.ReadLine());
+double z = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Pow(n, z);
+Console.WriteLine(result);
+

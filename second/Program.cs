8a2201abd267bb41809b5e550ec02e5a562a2c9a
arinach <arina.tchukurova@yandex.ru>
2022-09-01 10:26:44 +0300

    add secondtask

---
+++ second/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+int [] array = new int[8];
+Random rand = new Random();
+
+for (int i=0; i<array.Length; i++)
+   {
+    array[i] = rand.Next(0, 50);
+   }
+Console.WriteLine(string.Join(",", array));
+
+
